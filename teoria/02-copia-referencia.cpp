e296ca6d3b1da94b59b18434debeefca0ccdb39f
Inyte13 <luisgardaz13@gmail.com>
2026-04-12 20:59:55 -0500

    chore: por referencia, apunta al mismo espacio en memoria, con &

--- teoria/02-copia-referencia.cpp
+++ teoria/02-copia-referencia.cpp
@@ -9,3 +9,13 @@ int copia() {
   cout << "num2: " << num2 << "\n";
   return 0;
 }
+int referencia() {
+  cout << "Referencia, apunta las dos variables al mismo espacio en memoria "
+          "con el Operador de Referencia (&)\n";
+  int num1 = 100;
+  int& num2 = num1;
+  num2 = 39;
+  cout << "num1: " << num1 << "\n";
+  cout << "num2: " << num2 << "\n";
+  return 0;
+}
