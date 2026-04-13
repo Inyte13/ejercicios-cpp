c9e6114d5cd8dd8baf749c5f641e68ffc24be804
Inyte13 <luisgardaz13@gmail.com>
2026-04-12 20:59:17 -0500

    chore: por valor (me gusta decirle copia) sin &

--- teoria/02-copia-referencia.cpp
+++ teoria/02-copia-referencia.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 using namespace std;
+int copia() {
+  cout << "Copia, sin el Operador de Referencia (&)\n";
+  int num1 = 100;
+  int num2 = num1;
+  num2 = 39;
+  cout << "num1: " << num1 << "\n";
+  cout << "num2: " << num2 << "\n";
+  return 0;
+}
