2f74cf88fb91cbf683d363ab458beb04bda54b9d
Inyte13 <luisgardaz13@gmail.com>
2026-04-12 20:57:49 -0500

    chore: ejemplo de fn

--- teoria/01-introduccion.cpp
+++ teoria/01-introduccion.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <cassert>
 using namespace std;
+double pulgadasACentimetros(double pulgadas) {
+  if (pulgadas < 0) {
+    cerr << "La medida dada no puede ser negativa \n";
+    return -1.0;
+  } else {
+    return 2.54 * pulgadas;
+  }
+}
